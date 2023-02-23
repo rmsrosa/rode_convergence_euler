f3200f39bbf8f82d3a6c532778606f1257ef1e9d
Ricardo M. S. Rosa <rmsrosa@gmail.com>
2023-02-23 16:39:30 -0300

    safe indexing

--- src/solvers.jl
+++ src/solvers.jl
@@ -12,20 +12,20 @@ The values of `xt` are updated with the computed solution values.
 
 The time step is obtained from the length `N` of the vector `xt` via `dt = (tf - t0) / (N - 1)`.
 
-The noise vector `yt` is expected to be at least of the same length as `xt`.
+The noise vector `yt` should be of the same length as `xt`.
 """
-function solve_euler!(rng::AbstractRNG, xt::Vector{T}, t0::T, tf::T, x0::T, f::F, yt::Union{Vector{T}, SubArray{T, 1, Vector{T}, Tuple{StepRange{Int64, Int64}}, true}}) where {T, F}
-    N = length(yt)
-    length(xt) ≥ N || throw(
-        ArgumentError(
-            "Length of `xt` should be at least that of `yt` but got $(length(xt)) and $N"
-        )
+function solve_euler!(rng::AbstractRNG, xt::AbstractVector{T}, t0::T, tf::T, x0::T, f::F, yt::AbstractVector{T}) where {T, F}
+    axes(xt) == axes(yt) || throw(
+        DimensionMismatch("vectors `xt` and `yt` must match indices; got $(axes(xt)) and $(axes(yt)).")
     )
+    N = length(xt)
     dt = (tf - t0) / (N - 1)
-    xt[1] = x0
+    n1 = firstindex(xt)
+    xt[n1] = x0
     tn1 = t0
-    for n in 2:N
-        xt[n] = xt[n-1] + dt * f(tn1, xt[n-1], yt[n-1])
+    for n in Iterators.drop(eachindex(xt, yt), 1)
+        xt[n] = xt[n1] + dt * f(tn1, xt[n1], yt[n1])
+        n1 = n
         tn1 += dt
     end
 end
@@ -67,22 +67,22 @@ The values of `xt` are updated with the computed solution values.
 
 The time step is obtained from the length `N` of the vector `xt` via `dt = (tf - t0) / (N - 1)`.
 
-The noise vector `yt` is expected to be at least of the same length as `xt`.
+The noise vector `yt` should be of the same length as `xt`.
 """
 function solve_heun!(rng::AbstractRNG, xt::Vector{T}, t0::T, tf::T, x0::T, f::F, yt::Union{Vector{T}, SubArray{T, 1, Vector{T}, Tuple{StepRange{Int64, Int64}}, true}}) where {T, F}
-    N = length(yt)
-    length(xt) ≥ N || throw(
-        ArgumentError(
-            "Length of `xt` should be at least that of `yt` but got $(length(xt)) and $N"
-        )
+    axes(xt) == axes(yt) || throw(
+        DimensionMismatch("vectors `xt` and `yt` must match indices; got $(axes(xt)) and $(axes(yt)).")
     )
+    N = length(xt)
     dt = (tf - t0) / (N - 1)
-    xt[1] = x0
+    n1 = firstindex(xt)
+    xt[n1] = x0
     tn1 = t0
-    for n in 2:N
-        fn1 = f(tn1, xt[n-1], yt[n-1])
-        xtnaux = xt[n-1] + dt * fn1
+    for n in Iterators.drop(eachindex(xt, yt), 1)
+        fn1 = f(tn1, xt[n1], yt[n1])
+        xtnaux = xt[n1] + dt * fn1
         tn1 += dt
-        xt[n] = xt[n-1] + dt * (fn1 + f(tn1, xtnaux, yt[n])) / 2
+        xt[n] = xt[n1] + dt * (fn1 + f(tn1, xtnaux, yt[n])) / 2
+        n1 = n
     end
 end
