c5126e6e580f4999ed8c7c8084bfee5c3713b8d5
Ricardo Rosa <rmsrosa@gmail.com>
2023-03-23 17:33:07 -0300

    exploring

--- docs/literate/examples/08-earthquake.jl
+++ docs/literate/examples/08-earthquake.jl
@@ -1,31 +1,54 @@
-# # Seismic excitations of mechanical structures with the Kanai-Takimi Earthquake model
+# # Seismic excitations of mechanical structures with the Kanai-Takimi model
 
-# Now we consider a mechanical structure problem under ground-shaking excitations, based on the Kanai-Tajimi Earthquake model.
+# Now we consider a mechanical structure problem under ground-shaking excitations, based on the Kanai-Tajimi model.
 #
-# We consider a single-storey structure, with horizontal displacement $x$, which follows the equation
+# We consider a single-storey structure, with a random horizontal displacement $X_t$ relative to the ground motion displacement $Y_t$, which follows the equation
 # ```math
-#   \ddot x + 2\zeta\omega\dot x + \omega^2 x = y_t, \quad x(0) = \dot x(0) = 0,
+#   \ddot X_t + 2\zeta\omega\dot X_t + \omega^2 X_t = Y_t.
 # ```
-# where $\zeta$ and $\omega$ are damping and elastic model parameters depending on the soil conditions. Here, as in [Neckel & Rupp (2013)](https://doi.org/10.2478/9788376560267), based on [Housner & Jenning (1964)](https://doi.org/10.1061/JMCEA3.0000448), we use $\zeta = 0.64$ and $\omega = 15.56 \texttt{rad}/\texttt{s}$. The term $y_t$ is a stochastic noise representing the ground motion caused by the Earthquake and its aftershocks.
+# where $\zeta$ and $\omega$ are damping and elastic model parameters depending on the structure.
+#
+# For the numerical simulations, the second-order equation is written as a system of first-order equations:
+#
+# ```math
+#   \begin{cases}
+#       \dot X_t = V_t, \\
+#       \dot V_t = - \omega^2 X_t - 2\zeta\omega X_t + Y_t,
+#   \end{cases}
+# ```
+#
+# where $\{V_t\}_t$ is the random velocity relative to the ground.
+#
+# The term $Y_t$ is a stochastic noise representing the ground motion caused by the Earthquake and its aftershocks.
+#
+# The structure is originally at rest, before the first groundshaking excitations, so we have the conditions
+#
+# ```math
+# X_t(0) = 0, \quad \dot X_t(0) = 0.
+# ```
+#
+# When affected by the ground motion, the one-storey building has its ground at position $Y_t$ and its ceiling at position $Y_t + X_t$. The random process $X_t$ refers to the motion relative to the ground, and for which the damping and elastic forces are in effect.
+#
+# Here, as in [Neckel & Rupp (2013)](https://doi.org/10.2478/9788376560267), based on [Housner & Jenning (1964)](https://doi.org/10.1061/JMCEA3.0000448), we use $\zeta = 0.64$ and $\omega = 15.56 \texttt{rad}/\texttt{s}$.
 
 # Several types of noises have been considered in the literature. A typical one is a white noise. In practice, the noise is actually a colored noise, whose amplitude decays in time, followed by smaller peaks in amplitude due to aftershocks.
 #
 # With this in mind, we model the background colored noise with an Orsntein-Uhlenbeck (OU) process $\{O_t\}_t$ with a relatively small time scale $\tau$. More precisely, we assume $\{O_t\}_t$ satisfies the stochastic differential equation
 #
 # ```math
-#   \tau \mathrm{d}O_t = - \mathrm{d}t + D \mathrm{d}W_t,
+#   \tau \mathrm{d}O_t = - \mathrm{d}t + \sigma \mathrm{d}W_t,
 # ```
-# where $\{W_t\}_t$ is a standard Wiener process. This leads to an Orsntein-Uhlenbeck process with drift $\nu = 1/\tau$ and diffusion $\sigma = D/\tau$. This process has mean, variance, and covariance given by
+# where $\{W_t\}_t$ is a standard Wiener process. This leads to an Orsntein-Uhlenbeck process with drift $1/\tau$ and diffusion $\sigma/\tau$. This process has mean, variance, and covariance given by
 #
 # ```math
-#   \mathbb{E}[O_t] = O_0 e^{-\frac{\displaystyle t}{\displaystyle\tau}}, \quad \mathrm{Var}(O_t) = \frac{D^2}{2\tau}, \quad \mathrm{Cov}(O_t,O_s) = \frac{D^2}{2\tau} e^{-\frac{\displaystyle |t - s|}{\displaystyle \tau}}.
+#   \mathbb{E}[O_t] = O_0 e^{-\frac{\displaystyle t}{\displaystyle\tau}}, \quad \mathrm{Var}(O_t) = \frac{\sigma^2}{2\tau}, \quad \mathrm{Cov}(O_t,O_s) = \frac{\sigma^2}{2\tau} e^{-\frac{\displaystyle |t - s|}{\displaystyle \tau}}.
 # ```
 #
-# Hence, $O_t$ and $O_s$ are significantly correlated only when $|t - s| \lesssim \tau$. When $\tau \rightarrow 0$ with $D^2/2\tau \rightarrow 1$, this process approximates a Gaussian white noise.
+# Hence, $O_t$ and $O_s$ are significantly correlated only when $|t - s| \lesssim \tau$. When $\tau \rightarrow 0$ with $\sigma^2/2\tau \rightarrow 1$, this process approximates a Gaussian white noise.
 #
-# Moreover, in order to simulate the start of the first shock-wave and the subsequent aftershocks, we modulate the OU process with either a transport process or a self-excited point process.
+# Moreover, in order to simulate the start of the first shock-wave and the subsequent aftershocks, we modulate the Ornstein-Uhlenbeck process with either a transport process or a self-excited point process.
 #
-# The transport process is composed of a series of time-translations of a Hölder-continuous "attack" front, followed by an exponential decay: $\gamma (t - \delta)^\alpha e^{-\beta (t - \delta)}$, for $t \geq \delta$. The parameters $\alpha, \beta, \gamma, \delta$ are random parameters, with the Hölder exponent $\alpha$ being arbitrarily small. This is based on the model by [Bogdanoff, Goldberg & Bernard (1961)](https://doi.org/10.1785/BSSA0510020293), which has a linear attack rate, an exponential decay, and a specific combination of frequencies as the background noise.
+# The transport process is composed of a series of time-translations of a Hölder-continuous "attack" front, followed by an exponentially decaying kernel: $\gamma (t - \delta)^\alpha e^{-\beta (t - \delta)}$, for $t \geq \delta$. The parameters $\alpha, \beta, \gamma, \delta$ are random parameters, with the Hölder exponent $\alpha$ being arbitrarily small. This is based on the model by [Bogdanoff, Goldberg & Bernard (1961)](https://doi.org/10.1785/BSSA0510020293), which has a linear attack rate, an exponential decay, and a specific combination of frequencies as the background noise.
 #
 # The aftershocks, however, tend to come in clusters, with the ocurrence of an event increasing the chances for subsequent events. As such, self-exciting intensity processes have been successful in modeling the arrival times of the aftershocks (see e.g. [Pratiwi, Slamet, Saputro & Respatiwulan (2017)](https://doi.org/10.1088/1742-6596/855/1/012033)). The decaying kernel is usually an inverse power law, starting with the celebrated Omori formula [T. Utsu, Y. Ogata & R. S. Matsu'ura, The centenary of the Omori formula for a decay law of aftershock activity, Journal of Physics of the Earth, Volume 43 (1995), no. 1, 1-33](https://doi.org/10.4294/jpe1952.43.1)). Here, we choose to consider an exponentially decaying self-excited Hawkes process, which is easier to implement and suffices for illustrating the rate of convergence. Moreover, the intensity, or rate, of this inhomogenous Poisson point process, for the interarrival times, is not directly related to the magnitude of the aftershocks, but, again, for the sake of simplicity, we use the intensity itself as an envelope for the noise.
 #
@@ -42,14 +65,16 @@ using FFTW
 using Distributions
 using RODEConvergence
 
-# Then we set up some variables, starting by setting the random seed for reproducibility of the pseudo-random number sequence generator
+# Then we set up some variables, starting with the random seed, for reproducibility of the pseudo-random number sequence generator
 
 rng = Xoshiro(123)
 
-# We define the evolution law for the displacement $x$ driven by a noise $y$. Since it is a system of equations, we use the mutating form. Notice the noise is a product of the background colored noise `y[1]` and the envelope noise `y[2]`.
+# We define the evolution law for the displacement $X_t$ driven by a noise $Y_t$. Since it is a system of equations, we use the in-place formulation. Notice the noise is a product of the background colored noise `y[1]` and the envelope noise `y[2]`. The parameters are hard-coded for simplicity.
 
 function f!(dx, t, x, y)
     ζ = 0.64
+    ω = 15.56
+    ζ = 0.064
     ω = 15.56
     dx[1] = x[2]
     dx[2] = - 2 * ζ * ω * x[2] - ω ^ 2 * x[1] + y[1] * y[2]
@@ -60,7 +85,7 @@ end
 
 t0, tf = 0.0, 4.0
 
-# The structure is initially at rest, so the probability law is a Dirac delta function in $\mathbb{R}^2$:
+# The structure is initially at rest, so the probability law is a vectorial product of two Dirac delta distributions, in $\mathbb{R}^2$:
 
 x0law = product_distribution(Dirac(0.0), Dirac(0.0))
 
@@ -70,12 +95,10 @@ x0law = product_distribution(Dirac(0.0), Dirac(0.0))
 
 y0 = 0.0
 τ = 0.005 # time scale
-D = 0.1 # large-scale diffusion
-ν = 1/τ # drift
-σ = D/τ # diffusion
-colored_noise = OrnsteinUhlenbeckProcess(t0, tf, y0, ν, σ)
+σ = 0.1 # diffusion
+colored_noise = OrnsteinUhlenbeckProcess(t0, tf, y0, 1/τ, σ/τ)
 
-# Just for the sake of comparison, we also define a Wiener process, in order to obtain a white noise (by finite differences)
+# Just for the sake of comparison, we also define a Wiener process, in order to compare the colored noise with a white noise, obtained by finite differences on the Wiener process:
 
 wiener_noise = WienerProcess(t0, tf, y0)
 
@@ -86,7 +109,7 @@ nr = 8
 g(t, r) = mapreduce(ri -> ri[1] * max(0.0, t - ri[4]) ^ ri[2] * exp(-ri[3] * max(0.0, t - ri[4])), +, eachcol(r))
 transport_envelope_noise = TransportProcess(t0, tf, ylaw, g, nr)
 
-# Finally, we define the Hawkes process
+# Finally, we define the exponentially decaying Hawkes process
 
 λ₀ = 3.0
 a = 0.3
@@ -124,7 +147,7 @@ dt = (tf - t0) / (length(yt1) - 1)
 wt = (yt4[2:end] .- yt4[1:end-1])/dt^0.5
 nothing
 
-# Now we plot the Wiener process, the associated white noise, and the colored OU noise
+# Now we plot the Wiener process, the associated white noise, and the Ornstein-Uhlenbeck colored noise
 
 begin
     plot(xlabel="\$t\$", ylabel="\$\\mathrm{intensity}\$", guidefont=10)
@@ -153,7 +176,7 @@ println("Variance of the colored OU process: $(var(yt1))")
 
 nothing # hide
 
-# Now we plot the modulated noises.
+# Finally, we plot the modulated noises.
 
 begin
     plt1 = plot(xlabel="\$t\$", ylabel="\$\\mathrm{intensity}\$", guidefont=10)
@@ -171,8 +194,8 @@ end
 
 ntgt = 2^18
 ns = 2 .^ (6:9)
-nsample = ns[[1, 2, 3]]
-m = 1_000
+m = 1 # 1_000
+nothing # hide
 
 # We add some information about the simulation:
 
@@ -221,36 +244,81 @@ println(generate_error_table(hawkesmodulated_result, info_hawkes)) # hide
 nothing # hide
 
 # 
-# The calculated order of convergence is given by fieal `p`:
+# The calculated order of convergence is given by field `p`:
 
 println("Order of convergence `C Δtᵖ` with the transport-modulated noise with p = $(round(transportmodulated_result.p, sigdigits=2))")
 
 #
 
-
 println("Order of convergence `C Δtᵖ` with the Hawkes-modulated nooise with p = $(round(hawkesmodulated_result.p, sigdigits=2))")
 
-# 
 # 
 # ### Plots
 # 
 # We create plots with the rate of convergence with the help of a plot recipe for `ConvergenceResult`:
 
-plot(transportmodulated_result)
+plt_tr = plot(transportmodulated_result)
+plt_hk = plot(hawkesmodulated_result)
 
-# 
+plt = plot(plt_tr, plt_hk)
 
-plot(hawkesmodulated_result)
+# and we save them for the article:
 
-# 
-
-# savefig(plt, joinpath(@__DIR__() * "../../../../latex/img/", info.filename)) # hide
-# nothing # hide
+savefig(plt, joinpath(@__DIR__() * "../../../../latex/img/", "convergence_kanaitajimi.png")) # hide
+nothing # hide
 
 # For the sake of illustration, we plot a sample of an approximation of a target solution, in each case:
 
+nsample = ns[[1, 2, 3]]
 plot(transportmodulated_suite, ns=nsample)
 
 #
 
 plot(hawkesmodulated_suite, ns=nsample)
+
+# The displacement of the building is tiny compared to the ground, in this simulation
+
+plot(transportmodulated_suite, ns=nothing)
+plot!(transportmodulated_suite, xshow = false, yshow = prod, ns=nothing)
+
+# 
+
+plot(hawkesmodulated_suite, ns=nothing)
+plot!(hawkesmodulated_suite, xshow = false, yshow = prod, ns=nothing)
+
+# We also draw an animation of the motion of the single-storey building in each case. First the model with the transport-modulated noise.
+
+# And now with the Hawkes-modulated noise.
+
+dt = (tf - t0) / (ntgt - 1)
+
+@time anim = @animate for i in 1:div(ntgt, 2^12):div(ntgt, 2^4)
+    ground = prod(transportmodulated_suite.yt[i, :])
+    ceiling = ground + transportmodulated_suite.xt[i, 1]
+    heigth = 20.0
+    halfwidth = 10.0
+    plot([ground - halfwidth; ceiling - halfwidth; ceiling + halfwidth; ground + halfwidth], [0.0; heigth; heigth; 0.0], xlim = (-100.0, 100.0), ylim=(0.0, 100.0), xlabel="\$\\mathrm{displacement}\$", ylabel="\$\\textrm{height}\$", fill=true, title="Building at time t = $(round((i * dt), digits=3))", legend=false)
+end
+
+nothing # hide
+
+#
+
+gif(anim, fps = 30) # hide
+
+# Now just the relative motion
+
+@time anim = @animate for i in 1:div(ntgt, 2^12):div(ntgt, 2^4)
+    suite = transportmodulated_suite
+    ground = 0.0
+    ceiling = ground + suite.xt[i, 1]
+    heigth = 0.02
+    halfwidth = 0.001
+    plot([ground - halfwidth; ceiling - halfwidth; ceiling + halfwidth; ground + halfwidth], [0.0; heigth; heigth; 0.0], xlim = (-0.004, 0.004), ylim=(0.0, 0.1), xlabel="\$\\mathrm{displacement}\$", ylabel="\$\\textrm{height}\$", fill=true, title="Building at time t = $(round((i * dt), digits=3))", legend=false)
+end
+
+nothing # hide
+
+#
+
+gif(anim, fps = 30) # hide
