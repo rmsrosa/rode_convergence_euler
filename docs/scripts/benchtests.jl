c29d8c647adea8b0b24f5b074483b63f0351672a
Ricardo Rosa <rmsrosa@gmail.com>
2023-06-28 11:16:17 -0300

    clean up

--- docs/scripts/benchtests.jl
+++
@@ -1,5 +0,0 @@
-using BenchmarkTools
-
-nsteps, deltas, trajerrors, Yt, Xt, XNt = prepare_variables(Ntgt, Ns)
-
-@btime calculate_errors!($rng, $Yt, $Xt, $XNt, $X0, $f, $noise!, $target!, $trajerrors, $M, $t0, $tf, $Ns, $nsteps, $deltas)
