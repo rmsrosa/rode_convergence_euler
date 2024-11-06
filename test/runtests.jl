6042f78fad87a4ab9f339ce2e8b15c90cf1f9f01
Ricardo Rosa <rmsrosa@gmail.com>
2024-11-06 11:16:49 -0300

    back with all tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,8 @@ using Distributions, Plots, BenchmarkTools
 using RODEConvergence
 using Test
 
-#include("test_noises.jl")
-#include("test_solvers.jl")
+include("test_noises.jl")
+include("test_solvers.jl")
 include("test_convergence.jl")
 include("test_noalloc_conv.jl")
 include("test_plot_recipes.jl")
