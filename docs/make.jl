5bc26d853646c3d4c65d6f083a7d2d23e75ab59d
Ricardo Rosa <rmsrosa@gmail.com>
2025-08-23 10:40:30 -0300

    increase threshold

--- docs/make.jl
+++ docs/make.jl
@@ -61,8 +61,8 @@ include(joinpath(@__DIR__(), "literate.jl"))
         edit_link = "main",
         repolink = "https://github.com/rmsrosa/rode_convergence_euler",
         example_size_threshold = 100 * 2^10, # default is 8Kb, increase if necessary
-        size_threshold_warn = 200 * 2^10, # default is 100Kb; increase if necessary
-        size_threshold = 400 * 2^10, # default is 200Kb; increase if necessary
+        size_threshold_warn = 2^12, # default is 100Kb; increase if necessary
+        size_threshold = 2^14, # default is 200Kb; increase if necessary
     ),
     modules = [RODEConvergence],
 )
