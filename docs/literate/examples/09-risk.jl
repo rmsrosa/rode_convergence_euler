7c73c1a433283bc9188b211f933fa2250e7d066e
Ricardo Rosa <rmsrosa@gmail.com>
2025-08-25 20:33:32 -0300

    up m in risk

--- docs/literate/examples/09-risk.jl
+++ docs/literate/examples/09-risk.jl
@@ -147,7 +147,7 @@ nsample = ns[[1, 2, 3, 4]]
 
 # The number of simulations for the Monte Carlo estimate is set to
 
-m = 400
+m = 1400
 nothing # hide
 
 # And add some information about the simulation, for the caption of the convergence figure.
